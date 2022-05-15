bd9d079a54d8eb4a64b0771e34e72e7543ca9a3d
Liron Bdolah <bdolahliron@gmail.com>
2022-05-15 12:07:46 +0300

    Open-sourced

--- templates/main.js
+++ templates/main.js
@@ -1,14 +1,18 @@
-// navbar - highlight current page
-var current = document.getElementById('defult');
-  function highlight(link)
-  {
-     if (current != null)
-     {
-         current.className = "";
-     }
-     link.className = "highlight";
-     current = link;
+//pull the pathname from window location
+const activePage = window.location.pathname;
+console.log(window);
+console.log(window.location);
+console.log(activePage);
+
+/*create an arey of the links in nav, 
+compare each to pathname and mark the one that is active
+*/ 
+const navLinks = document.querySelectorAll('nav a').forEach(link => {    
+  if(link.href.includes(`${activePage}`)){
+    link.classList.add('active');
   }
+});
+
 
   // calculate age
   function birthdayalert() {
